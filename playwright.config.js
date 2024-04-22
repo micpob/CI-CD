0425c11ca5eee0456b6226c8f4b09fe1ba980361
micpob <bomboloapp@gmail.com>
2024-04-22 11:32:01 +0200

    Exercise 11.9

--- playwright.config.js
+++ playwright.config.js
@@ -1,5 +1,5 @@
 // @ts-check
-const { defineConfig, devices } = require('@playwright/test');
+const { defineConfig, devices } = require('@playwright/test')
 
 /**
  * Read environment variables from file.
@@ -25,7 +25,7 @@ module.exports = defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-     baseURL: 'http://127.0.0.1:8080',
+    baseURL: 'http://127.0.0.1:8080',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -77,10 +77,9 @@ module.exports = defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-   /* webServer: {
+  /* webServer: {
      command: 'npm run start',
      url: 'http://127.0.0.1:3000',
      reuseExistingServer: !process.env.CI,
    }, */
-});
-
+})
